f96ac96b36c83460bcd5e25d9f43ada8ef6f98ea
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2021-04-28 15:42:32 +0200

    Add wuzzuf background

--- src/component/form/registerForm.jsx
+++ src/component/form/registerForm.jsx
@@ -1,8 +1,23 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core";
 import DescriptionContainer from "./descriptionContainer";
 import RegisterContainer from "./registerContainer";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    // margin: theme.spacing(3),
+    display: "flex",
+    flexWrap: "wrap",
+    padding: theme.spacing(4),
+
+    width: "100%",
+    "&>*": {
+      transition: ".3s",
+    },
+  },
+}));
 const RegisterForm = () => {
+  const classes = useStlyes();
   return (
     <Grid>
       <DescriptionContainer /> <RegisterContainer />
