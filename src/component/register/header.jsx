788fa2b085103e2e3d6cc4d10b0c8c4e6a4d80c5
AmrAhmedA <47694180+AmrAhmedA@users.noreply.github.com>
2021-04-28 16:52:55 +0200

    Add register button css + extra global css

--- src/component/register/header.jsx
+++ src/component/register/header.jsx
@@ -1,11 +1,44 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core";
 import WUZZUFLOGO from "../../assets/wuzzufLogo/wuzzufLogoSVG";
-const RegisterHeader = ({ classes }) => {
+
+const useStyles = makeStyles((theme) => ({
+  registerButton: {
+    fontSize: "15px",
+    padding: "0 12px",
+    border: "1px solid transparent",
+    borderRadius: "2px",
+    textAlign: "center",
+    WebkitUserSelect: "none",
+    cursor: "pointer",
+    minWidth: "80px",
+    WebkitTransition: "color .2s",
+    minHeight: "32px",
+    lineHeight: "22px",
+    color: "#ffffff",
+    backgroundColor: "#2C9266",
+    textDecorationLine: "none",
+    display: "flex",
+    justify: "center",
+    alignItems: "center",
+
+    "&:hover": {
+      backgroundColor: "#2f745a",
+      transition: ".3s ",
+    },
+  },
+}));
+const RegisterHeader = () => {
+  const classes = useStyles();
   return (
     <Grid container direction="row" justify="space-between" alignItems="center">
       <WUZZUFLOGO color="white" />
-      <button>Hello World</button>
+      <a
+        href="https://www.facebook.com/amrahmedgewaly/"
+        className={classes.registerButton}
+      >
+        Employer?
+      </a>
     </Grid>
   );
 };
